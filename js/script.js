5e9e8892159d49647a7d71424f5e6b1af3a659d6
FilippoFoti <filippofoti7@gmail.com>
2023-03-14 15:32:34 +0100

    Milestone 3 completato

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// Adesso rimuoviamo tutto il markup statico e inseriamo tutte le 
-// immagini dinamicamente servendoci dell'array fornito e un semplice 
-// ciclo for che concatena un template literal.
-// Tutte le immagini saranno nascoste, tranne la prima, che avrà una 
-// classe specifica che la renderà visibile.
-// Al termine di questa fase ci ritroveremo con lo stesso slider stilato 
-// nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-
 // Inserisco le immagini dinamiche nell'array
 const imgArray = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg"];
 
@@ -15,17 +6,67 @@ const itemsContainer = document.querySelector(".slider-items");
 for (let i = 0; i < imgArray.length; i++) {
     const currentImg = imgArray [i];
 
-    const sliderItem = `
-        <div class = "item">
+    const sliderItem = 
+        `<div class = "item">
             <img src="${currentImg}" alt="">
         </div>`;
 
     itemsContainer.innerHTML += sliderItem;
 }
 
+// Imposto la prima immagine e nascondo le altre in css
 const itemsArray = document.getElementsByClassName("item");
 console.log(itemsArray);
 
-
 let activeItemIndex = 0;
 itemsArray[activeItemIndex].classList.add("active");
+
+
+// Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva,
+// che quindi verrà visualizzata al posto della precedente.
+
+
+// Click primo bottone
+const nextBtn = document.querySelector(".next");
+nextBtn.addEventListener("click", function() {
+    prevBtn.classList.remove("hidden");
+    if (activeItemIndex < (itemsArray.length - 1)) {
+        // Rimuovo active dall'img precedente
+        itemsArray[activeItemIndex].classList.remove("active");
+
+        // Incremento
+        activeItemIndex++;
+
+        // Aggiungo active alla nuova img
+        itemsArray[activeItemIndex].classList.add("active");
+
+        // Rimuovo il bottone quando arrivo all'ultima img
+        if (activeItemIndex === itemsArray.length - 1) {
+            nextBtn.classList.add("hidden");
+        }
+    }
+});
+
+
+// Click secondo bottone 
+const prevBtn = document.querySelector(".prev");
+prevBtn.classList.add("hidden");
+
+prevBtn.addEventListener("click", function () {
+    // Rimuovo l'hidden dal bottone
+    nextBtn.classList.remove("hidden");
+
+    // Rimuovo active
+    itemsArray[activeItemIndex].classList.remove("active");
+
+    // Decremento
+    activeItemIndex--;
+
+    // aggiungo active alla nuova img
+    itemsArray[activeItemIndex].classList.add("active");
+
+    if (activeItemIndex === 0) {
+        prevBtn.classList.add("hidden");
+    }
+});
+
